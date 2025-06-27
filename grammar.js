502d6a0647234c35cf033f1837a08ca1ba49753f
Ciaran Dunne <cciaranddunne@gmail.com>
2025-06-27 03:22:56 +0100

    another one

--- grammar.js
+++ grammar.js
@@ -1,40 +1,148 @@
-// grammar.js - Tree-sitter grammar for LambdaPi
-
 module.exports = grammar({
   name: "lambdapi",
+
+  extras: ($) => [$.comment, /\s/],
+
   conflicts: ($) => [
-    [$.path, $.qid],
-    [$.qid, $.qid_expl],
-    [$.tactic, $.query],
+    [$.term, $.saterm],
+    [$.aterm, $.term_id],
+    [$.param, $.uid],
   ],
 
-  extras: ($) => [/\s/, $.comment],
-  word: ($) => $.uid,
-
   rules: {
-    source_file: ($) => repeat($._command),
+    source_file: ($) => repeat($.command),
 
     // Comments
-    comment: ($) => token(choice(seq("//", /.*/), seq("/*", /[\s\S]*?/, "*/"))),
+    comment: ($) =>
+      choice(seq("//", /.*/), seq("/*", /[^*]*\*+([^/*][^*]*\*+)*/, "/")),
 
-    // Identifiers
-    uid: ($) => /[$A-Za-z_][A-Za-z0-9_]*/,
-    esc_uid: ($) => seq("{|", $.uid, "|}"),
-    qid: ($) => seq($.uid, repeat(seq(".", $.uid))),
+    // Commands
+    command: ($) =>
+      choice(
+        $.opaque_command,
+        $.require_command,
+        $.require_as_command,
+        $.open_command,
+        $.symbol_command,
+        $.inductive_command,
+        $.rule_command,
+        $.builtin_command,
+        $.coerce_rule_command,
+        $.unif_rule_command,
+        $.notation_command,
+        $.query_command,
+      ),
 
-    // Paths
-    path: ($) => choice($.uid, $.qid),
+    opaque_command: ($) => seq("opaque", $.qid, ";"),
 
-    // Basic tokens
-    integer: ($) => /[0-9]+/,
-    string: ($) => token(seq('"', /(?:\\.|[^"\\])*?/, '"')),
+    require_command: ($) =>
+      seq("require", optional("open"), repeat1($.path), ";"),
 
-    // Switches, modifiers, etc.
-    switch: ($) => choice("on", "off"),
+    require_as_command: ($) => seq("require", $.path, "as", $.uid, ";"),
+
+    open_command: ($) => seq("open", repeat1($.path), ";"),
+
+    symbol_command: ($) =>
+      seq(
+        repeat($.modifier),
+        "symbol",
+        $.uid,
+        repeat($.param_list),
+        choice(
+          seq(":", $.term, optional($.proof), ";"),
+          seq(optional(seq(":", $.term)), "≔", $.term_proof, ";"),
+        ),
+      ),
+
+    inductive_command: ($) =>
+      seq(
+        optional($.exposition),
+        repeat($.param_list),
+        "inductive",
+        sep1($.inductive_def, "with"),
+        ";",
+      ),
+
+    inductive_def: ($) =>
+      seq(
+        $.uid,
+        repeat($.param_list),
+        ":",
+        $.term,
+        "≔",
+        optional("|"),
+        sep($.constructor, "|"),
+      ),
+
+    constructor: ($) => seq($.uid, repeat($.param_list), ":", $.term),
+
+    rule_command: ($) => seq("rule", sep1($.rule, "with"), ";"),
+
+    rule: ($) => seq($.term, "↪", $.term),
+
+    builtin_command: ($) => seq("builtin", $.string, "≔", $.qid, ";"),
+
+    coerce_rule_command: ($) => seq("coerce_rule", $.rule, ";"),
+
+    unif_rule_command: ($) => seq("unif_rule", $.unif_rule, ";"),
+
+    unif_rule: ($) => seq($.equation, "↪", "[", sep1($.equation, ";"), "]"),
+
+    equation: ($) => seq($.term, "≡", $.term),
+
+    notation_command: ($) => seq("notation", $.qid, $.notation, ";"),
+
+    notation: ($) =>
+      choice(
+        seq("infix", optional($.side), $.priority),
+        seq("postfix", $.priority),
+        seq("prefix", $.priority),
+        "quantifier",
+      ),
+
     side: ($) => choice("left", "right"),
-    assert: ($) => choice("assert", "assertnot"),
-    exposition: ($) => choice("private", "protected"),
+    priority: ($) => choice($.int, $.float),
 
+    query_command: ($) => seq($.query, ";"),
+
+    query: ($) =>
+      choice(
+        $.assert_query,
+        $.compute_query,
+        $.print_query,
+        $.proofterm_query,
+        $.debug_query,
+        $.flag_query,
+        $.prover_query,
+        $.prover_timeout_query,
+        $.verbose_query,
+        $.type_query,
+        $.search_query,
+      ),
+
+    assert_query: ($) =>
+      seq(
+        choice("assert", "assertnot"),
+        repeat($.param_list),
+        "⊢",
+        $.term,
+        choice(seq(":", $.term), seq("≡", $.term)),
+      ),
+
+    compute_query: ($) => seq("compute", $.term),
+    print_query: ($) => seq("print", optional($.qid_or_rule)),
+    proofterm_query: ($) => "proofterm",
+    debug_query: ($) => seq(choice("+", "-"), /[a-z]+/),
+    flag_query: ($) => seq("flag", $.string, choice("on", "off")),
+    prover_query: ($) => seq("prover", $.string),
+    prover_timeout_query: ($) => seq("prover_timeout", $.int),
+    verbose_query: ($) => seq("verbose", $.int),
+    type_query: ($) => seq("type", $.term),
+    search_query: ($) => seq("search", $.string),
+
+    qid_or_rule: ($) => choice($.qid, "unif_rule", "coerce_rule"),
+
+    // Modifiers
     modifier: ($) =>
       choice(
         seq(optional($.side), "associative"),
@@ -46,8 +154,9 @@ module.exports = grammar({
         $.exposition,
       ),
 
+    exposition: ($) => choice("private", "protected"),
+
     // Parameters
-    param: ($) => choice($.uid, "_"),
     param_list: ($) =>
       choice(
         $.param,
@@ -55,14 +164,15 @@ module.exports = grammar({
         seq("[", repeat1($.param), optional(seq(":", $.term)), "]"),
       ),
 
+    param: ($) => choice($.uid, "_"),
+
     // Terms
-    term: ($) => $._term,
-    _term: ($) =>
+    term: ($) =>
       choice(
         $.bterm,
         $.saterm,
-        seq($.saterm, $.bterm),
-        seq($.saterm, "→", $.term),
+        prec.left(1, seq($.saterm, $.bterm)),
+        prec.right(2, seq($.saterm, "→", $.term)),
       ),
 
     bterm: ($) =>
@@ -70,17 +180,50 @@ module.exports = grammar({
         seq("`", $.term_id, $.binder),
         seq("Π", $.binder),
         seq("λ", $.binder),
-        seq(
-          "let",
-          $.uid,
-          repeat($.param_list),
-          optional(seq(":", $.term)),
-          "≔",
-          $.term,
-          "in",
-          $.term,
-        ),
+        $.let_term,
       ),
+
+    let_term: ($) =>
+      seq(
+        "let",
+        $.uid,
+        repeat($.param_list),
+        optional(seq(":", $.term)),
+        "≔",
+        $.term,
+        "in",
+        $.term,
+      ),
+
+    saterm: ($) => prec.left(seq($.saterm, $.aterm)),
+
+    aterm: ($) =>
+      choice(
+        $.term_id,
+        "_",
+        "TYPE",
+        $.meta_var,
+        $.pattern_var,
+        $.wrapped_term,
+        $.explicit_term,
+        $.int,
+        $.string,
+      ),
+
+    meta_var: ($) => seq("?", $.int, optional($.env)),
+    pattern_var: ($) => seq("$", choice($.identifier, $.int), optional($.env)),
+    wrapped_term: ($) => seq("(", $.term, ")"),
+    explicit_term: ($) => seq("[", $.term, "]"),
+
+    env: ($) => seq(".", "[", sep($.term, ";"), "]"),
+
+    term_id: ($) => choice($.qid, $.qid_expl),
+
+    qid: ($) => choice($.identifier, $.qualified_id),
+
+    qid_expl: ($) => choice(seq("@", $.identifier), seq("@", $.qualified_id)),
+
+    qualified_id: ($) => seq(repeat1(seq($.id_part, ".")), $.id_part),
 
     binder: ($) =>
       choice(
@@ -88,34 +231,25 @@ module.exports = grammar({
         seq($.param, ":", $.term, ",", $.term),
       ),
 
-    saterm: ($) => repeat1($.aterm),
-    aterm: ($) =>
-      choice(
-        $.term_id,
-        "_",
-        "TYPE",
-        seq("?", $.uid, optional($.env)),
-        seq("$", $.uid, optional($.env)),
-        seq("(", $.term, ")"),
-        seq("[", $.term, "]"),
-        $.integer,
-        $.string,
-      ),
-    env: ($) =>
-      seq(".", "[", optional(seq($.term, repeat(seq(";", $.term)))), "]"),
-
-    term_id: ($) => choice($.qid, $.qid_expl),
-    qid_expl: ($) => seq("@", choice($.uid, $.qid)),
-
     term_proof: ($) => choice($.term, $.proof, seq($.term, $.proof)),
 
     // Proofs
-    proof: ($) => seq("begin", repeat1($.subproof), $.proof_end),
+    proof: ($) =>
+      seq(
+        "begin",
+        choice(repeat1($.subproof), optional($.proof_steps)),
+        $.proof_end,
+      ),
+
     subproof: ($) => seq("{", optional($.proof_steps), "}"),
-    proof_steps: ($) => seq($.proof_step, optional(seq(";", $.proof_steps))),
+
+    proof_steps: ($) => sep1($.proof_step, ";"),
+
     proof_step: ($) => seq($.tactic, repeat($.subproof)),
+
     proof_end: ($) => choice("abort", "admitted", "end"),
 
+    // Tactics
     tactic: ($) =>
       choice(
         $.query,
@@ -132,19 +266,23 @@ module.exports = grammar({
         "reflexivity",
         seq("remove", repeat1($.uid)),
         seq("repeat", $.tactic),
-        seq("rewrite", optional($.side), optional($.rw_patt_spec), $.term),
+        $.rewrite_tactic,
         seq("set", $.uid, "≔", $.term),
-        "simplify",
-        seq("simplify", $.qid),
-        seq("simplify", "rule", "off"),
+        $.simplify_tactic,
         "solve",
         "symmetry",
         seq("try", $.tactic),
         seq("why3", optional($.string)),
-        // Debug logs: any sequence of characters after + or -
-        seq("debug", choice("+", "-"), token(/.+/)),
       ),
+
+    rewrite_tactic: ($) =>
+      seq("rewrite", optional($.side), optional($.rw_patt_spec), $.term),
+
+    simplify_tactic: ($) =>
+      seq("simplify", optional(choice($.qid, seq("rule", "off")))),
+
     rw_patt_spec: ($) => seq(".", "[", $.rw_patt, "]"),
+
     rw_patt: ($) =>
       choice(
         $.term,
@@ -154,100 +292,40 @@ module.exports = grammar({
         seq($.term, "as", $.uid, "in", $.term),
       ),
 
-    // Inductive, constructors
-    inductive: ($) =>
-      seq(
-        $.uid,
-        repeat($.param_list),
-        ":",
-        $.term,
-        "≔",
-        optional("|"),
-        sepBy1("|", $.constructor),
-      ),
-    constructor: ($) => seq($.uid, repeat($.param_list), ":", $.term),
+    // Paths and identifiers
+    path: ($) => $.qualified_id,
 
-    // Rules
-    clause: ($) => seq($.term, "↪", $.term),
-    rule: ($) => seq($.clause, repeat(seq("with", $.clause))),
-    unif_rule: ($) => seq($.equation, "↪", "[", sepBy1(";", $.equation), "]"),
-    equation: ($) => seq($.term, "≡", $.term),
+    uid: ($) => $.identifier,
 
-    notation: ($) =>
-      choice(
-        seq("infix", optional($.side), $.float_or_int),
-        seq("postfix", $.float_or_int),
-        seq("prefix", $.float_or_int),
-        "quantifier",
-      ),
-    float_or_int: ($) => choice($.integer, /[0-9]+\.[0-9]+/),
+    identifier: ($) => choice($.regular_id, $.escaped_id),
 
-    // Queries
-    query: ($) =>
-      choice(
-        seq($.assert, repeat($.param_list), "⊢", $.term, ":", $.term),
-        seq($.assert, repeat($.param_list), "⊢", $.term, "≡", $.term),
-        seq("compute", $.term),
-        seq("print", optional($.qid_or_rule)),
-        "proofterm",
-        // Debug: allow any trailing text
-        seq("debug", choice("+", "-"), token(/.+/)),
-        seq("flag", $.string, $.switch),
-        seq("prover", $.string),
-        seq("prover_timeout", $.integer),
-        seq("verbose", $.integer),
-        seq("type", $.term),
-        seq("search", $.string),
-      ),
-    qid_or_rule: ($) => choice($.qid, "unif_rule", "coerce_rule"),
+    id_part: ($) => choice($.regular_id, $.escaped_id),
 
-    // Commands
-    _command: ($) =>
-      choice(
-        seq("opaque", $.qid, ";"),
-        seq("require", "open", repeat($.path), ";"),
-        seq("require", repeat($.path), ";"),
-        seq("require", $.path, "as", $.uid, ";"),
-        seq("open", repeat($.path), ";"),
-        seq(
-          repeat($.modifier),
-          "symbol",
-          $.uid,
-          repeat($.param_list),
-          ":",
-          $.term,
-          optional($.proof),
-          ";",
-        ),
-        seq(
-          repeat($.modifier),
-          "symbol",
-          $.uid,
-          repeat($.param_list),
-          optional(seq(":", $.term)),
-          "≔",
-          $.term_proof,
-          ";",
-        ),
-        seq(
-          optional($.exposition),
-          repeat($.param_list),
-          "inductive",
-          $.inductive,
-          repeat(seq("with", $.inductive)),
-          ";",
-        ),
-        seq("rule", $.rule, ";"),
-        seq("builtin", $.string, "≔", $.qid, ";"),
-        seq("coerce_rule", $.rule, ";"),
-        seq("unif_rule", $.unif_rule, ";"),
-        seq("notation", $.qid, $.notation, ";"),
-        seq($.query, ";"),
-      ),
+    regular_id: ($) => /[^,;\r\t\n(){}\[\]:.`"@$|\/\s]+/,
+
+    escaped_id: ($) => /\{\|[^|]*\|(\|[^|}]+\|)*\}/,
+
+    // Literals
+    int: ($) => /-?[0-9]+/,
+    float: ($) => /-?[0-9]+\.[0-9]+/,
+    string: ($) => /"[^"]*"/,
+
+    // Unicode symbols (alternatives to ASCII)
+    arrow: ($) => choice("→", "->"),
+    assign: ($) => choice("≔", ":="),
+    equiv: ($) => choice("≡", "=="),
+    hook_arrow: ($) => choice("↪", "|->"),
+    lambda: ($) => choice("λ", "\\"),
+    pi: ($) => choice("Π", "forall"),
+    turnstile: ($) => choice("⊢", "|-"),
   },
 });
 
-// Helper: sequence separated by a delimiter
-function sepBy1(delim, rule) {
-  return seq(rule, repeat(seq(delim, rule)));
+// Helper function for separated lists
+function sep(rule, separator) {
+  return optional(sep1(rule, separator));
+}
+
+function sep1(rule, separator) {
+  return seq(rule, repeat(seq(separator, rule)));
 }
